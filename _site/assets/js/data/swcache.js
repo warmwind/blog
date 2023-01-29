029e6551f24073696d5bb0366a957669c21880fe
Oscar Jiang <pengj0520@gmail.com>
2023-01-29 23:51:23 +0800

    Update to use correct host

--- _site/assets/js/data/swcache.js
+++ _site/assets/js/data/swcache.js
@@ -44,7 +44,7 @@ const resource = [
 const allowedDomains = [
     
 
-    'localhost:4000',
+    'www.oscarjiang.site',
 
     
 
--- _site/sw.js
+++ _site/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20230129.233837';
+const cacheName = 'chirpy-20230129.235332';
 
 function verifyDomain(url) {
     for (const domain of allowedDomains) {
