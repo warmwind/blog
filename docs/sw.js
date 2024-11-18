31327650723dd9385c87475eefe666429ab347f1
Oscar Jiang <pengj0520@gmail.com>
2024-11-18 11:37:31 +0800

    Remove seo test

--- docs/sw.js
+++ docs/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20241118.113519';
+const cacheName = 'chirpy-20241118.113656';
 
 function verifyDomain(url) {
     for (const domain of allowedDomains) {
