bad91ec865942df36b73ba5268b4b6cfc401fc91
imjposcar <imjposcar@gmail.com>
2026-03-18 23:40:27 -0700

    Remove scheduled post preview margin

--- src/config.ts
+++ src/config.ts
@@ -8,7 +8,7 @@ export const SITE = {
   lightAndDarkMode: true,
   postPerIndex: 4,
   postPerPage: 20,
-  scheduledPostMargin: 15 * 60 * 1000, // 15 minutes
+  scheduledPostMargin: 0,
   showBackButton: true,
   editPost: {
     enabled: false,
